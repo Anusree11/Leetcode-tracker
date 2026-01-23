310f48511712b62be20e0af32b153808c62459d0
Anusree11 <anusree1598@gmail.com>
2026-01-23 20:11:31 +0530

    DB Updated

--- tracker.py
+++ tracker.py
@@ -1,31 +1,91 @@
-import csv
+from flask import Flask, request , jsonify
+import sqlite3
 from datetime import date
 
-FILE_NAME = "leetcode_tracker.csv"
+app = Flask(__name__)
+DB_NAME= "leetcode_tracker.db"
 
-def add_problem():
-    problem_name = input("Problem name: ")
-    difficulty = input("Difficulty (Easy/Medium/Hard): ")
-    topic = input("Topic (Array/String/etc): ")
 
-    with open(FILE_NAME, "a", newline="") as file:
-        writer = csv.writer(file)
-        writer.writerow([date.today(), problem_name, difficulty, topic])
+def get_db_connection():
+    db_conn= sqlite3.connect(DB_NAME)
+    db_conn.row_factory = sqlite3.Row
+    return db_conn
 
-    print("Problem added succesfully")
 
-def menu():
-    print("\nLeetCode Tracker")
-    print("1. Add problem")
-    print("2. Exit")
+def create_table():
+    db_conn= get_db_connection()
+    cursor= db_conn.execute("""
+         CREATE TABLE IF NOT EXISTS leetcode_problems(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            data_solved TEXT,
+            problem_number INTEGER,
+            problem_name TEXT,
+            difficulty TEXT,
+            Notes Text
+     )
+""")
+    
+    db_conn.commit()
+    db_conn.close()
 
-    choice = input("Choose an option: ")
+    @tracker.route("/add", methods=["POST"])
+    def add_problem():
+        data= get_data.get_json()
 
-    if choice == "1":
-        add_problem()
-    elif choice == "2":
-        print("Goodbye!")
-    else:
-        print("Invalid choice")
+        problem_name=data.get("problem_name")
+        problem_number=data.get("problem_number")
+        difficulty = data.get("difficulty")
+        topic= data.get("topic")
+        Notes=data.get("Notes")
 
-menu()
+        if not problem_name or not difficulty or not topic or not Notes:
+            return jsonify({"error": "All the fields are required"}), 400
+        
+
+        db_conn=get_db_connection()
+        cursor= db_conn.cursor()
+        cursor.execute("""
+             INSERT INTO leetcode_problems(date_solved, problem_name, problem_number, difficulty,topic, Notes)
+                       VALUES (?,?,?,?,?)"""), (date.today().isoformat(), problem_name,problem_number,difficulty,topic,Notes)
+        
+        db_conn.commit()
+        db_conn.close()
+
+        return jsonify({"Message":"Problem added succesfully"})
+    
+
+    @tracker.route("/problems", methods=["GET"])
+    def get_problems():
+        problem_number=request.args.get("problem_number")
+        problem_name=request.args.get("problem_name")
+
+        db_conn= get_db_connection()
+        cursor= db_conn.cursor()
+
+        if problem_number:
+            cursor.execute(
+                "SELECT * FROM leetcode_problems WHERE problem_number=?",
+                (problem_number,)
+            )
+            rows=cursor.fetchall()
+
+        elif problem_name:
+            cursor.execute(
+                "SELECT * FROM leetcode_problems WHERE problem_name LIKE ?",
+                (f"%{problem_name}")
+            )
+            rows=cursor.fetchall()
+
+        else:
+            cursor.execute("SELECT * FROM leetcode_problems")
+            rows = cursor.fetchall()
+
+
+
+        conn.close()
+
+        problems=[dict(row) for row in rows]
+        return jsonify(problems), 200
+
+        
+
